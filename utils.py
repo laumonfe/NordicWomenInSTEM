141b567032b20cc7b9e98bf321fc71b87212c4f4
laumonfe <laumonfe@hotmail.com>
2023-06-11 20:20:49 +0200

    f

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def show_img(img_type):
     if img_type == "scale":
         path1 = "/content/NordicWomenInSTEM/images/Height.png"
         path2 = "/content/NordicWomenInSTEM/images/Width.png"
-     else:
+    else:
         path1 = "/content/NordicWomenInSTEM/images/moveX.png"
         path2 = "/content/NordicWomenInSTEM/images/moveY.png"
 
