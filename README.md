9630c0e113aca156fed92d4930ac3b4d44d601de
Laura Montesdeoca Fenoy <laumonfe@hotmail.com>
2024-10-01 14:00:47 +0200

    Updated collab

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NordicWomenInSTEM
 
-Click here! 
+## Click here! 
 <a target="_blank" href="https://colab.research.google.com/github/laumonfe/NordicWomenInSTEM/blob/main/funMath_collab.ipynb">
   <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
 </a>
--- utils.py
+++ utils.py
@@ -186,6 +186,20 @@ def move_square(M=np.float32([[1, 0, 0],
 
     newpoints = np.array(newpoints)
     plot_points(target, newpoints, target_color, default_color)
+    if (target_color == 'tomato') or (target_color == 'limegreen') 
+        if target == newpoints:
+            congrats = plt.imread("/content/NordicWomenInSTEM/images/congratulations.jpg")
+            plt.imshow(congrats)
+            plt.title("Wow, perfect!")
+            plt.axis('off')
+            plt.show()
+        
+    elif (target_color == default_color and target == newpoints):
+        congrats = plt.imread("/content/NordicWomenInSTEM/images/congratulations.jpg")
+        plt.imshow(congrats)
+        plt.title("Wow, perfect!")
+        plt.axis('off')
+        plt.show()
 
 
 def excercise1(M):
