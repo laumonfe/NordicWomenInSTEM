1460234e9a26ac0e7b7f1067d9fd5ae48f40c3a1
Andreea Stefan <andreea.stefan@3shape.com>
2025-05-23 15:29:56 +0200

    Clone only if the repo dosent exist

--- infrastructure/entrypoint.sh
+++ infrastructure/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-# Always clone fresh
-cd /home/jovyan/work
-rm -rf NordicWomenInSTEM
-git clone https://github.com/laumonfe/NordicWomenInSTEM.git
-echo "NordicWomenInSTEM repository cloned freshly."
+# If the NordicWomenInSTEM directory doesn't exist, clone the repository
+if [ ! -d "/home/jovyan/work/NordicWomenInSTEM" ]; then
+    cd /home/jovyan/work
+    git clone https://github.com/laumonfe/NordicWomenInSTEM.git
+    echo "NordicWomenInSTEM repository cloned successfully."
+fi
 
 # Start JupyterLab without authentication for easy sharing
 exec start-notebook.sh --NotebookApp.token='' --NotebookApp.password='' --ip=0.0.0.0 "$@"
